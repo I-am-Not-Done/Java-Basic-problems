7cb228117ee1f9fa3a08fba3d4eee5800b4416ab
ABHISHEK ALLAMPELLI <abhishekallampelli123@gmail.com>
2025-12-19 14:01:44 +0530

    Add files via upload

---
+++ SortArray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class SortArray {
+    public static void main(String[] args) {
+        int array1[] = {15, 25, 23, 19, 20};
+        Arrays.sort(array1);
+        System.out.println("Sorted array: " + Arrays.toString(array1));
+    }
+}
